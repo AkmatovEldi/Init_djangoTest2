43effb209bc9afdcdc256852b64418e5ba866cda
AkmatovEldi <akmatoveldi@gmail.com>
2021-03-11 09:28:04 +0600

    Debug

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'fldd5_vrb93qczhmfaig6auo=v&q18vik4irmv_rctn@4rb)ux'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
